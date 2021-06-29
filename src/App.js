3c39c0f73ad844d4479d77b73d9fc462731a75a8
ibrahemomari <ibrahem.omari96@gmail.com>
2021-06-29 22:52:18 +0300

    lab8

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import City from './City'
+
 class App extends Component {
+  
   render() {
     return (
       <div>
